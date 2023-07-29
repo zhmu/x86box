490bf956bb3c811c707ae4c11ccf607fe79233b5
Rink Springer <rink@rink.nu>
2023-07-29 15:39:57 +0200

    Restructue VGA code a bit

--- src/vga.cpp
+++ src/vga.cpp
@@ -4,6 +4,7 @@
 
 #include "io.h"
 #include "hostio.h"
+#include "memory.h"
 #include "vgafont.h"
 
 #include "spdlog/spdlog.h"
@@ -35,59 +36,110 @@ namespace
     }
 }
 
-VGA::VGA(Memory& memory, IO& io, HostIO& hostio)
-    : m_memory(memory), m_hostio(hostio), m_io(io)
+struct VGA::Impl : XMemoryMapped, IOPeripheral
 {
-    m_videomem = std::make_unique<uint8_t[]>(VideoMemorySize);
+    HostIO& hostio;
+    std::array<uint8_t, VideoMemorySize> videomem{};
 
+    Impl(Memory& memory, IO& io, HostIO& hostio);
+
+    uint8_t ReadByte(Memory::Address addr) override;
+    uint16_t ReadWord(Memory::Address addr) override;
+
+    void WriteByte(Memory::Address addr, uint8_t data) override;
+    void WriteWord(Memory::Address addr, uint16_t data) override;
+
+    void Out8(io_port port, uint8_t val) override;
+    void Out16(io_port port, uint16_t val) override;
+    uint8_t In8(io_port port) override;
+    uint16_t In16(io_port port) override;
+
+
+    void Update();
+};
+
+
+VGA::Impl::Impl(Memory& memory, IO& io, HostIO& hostio)
+    : hostio(hostio)
+{
     memory.AddPeripheral(0xa0000, 65535, *this);
     memory.AddPeripheral(0xb0000, 65535, *this);
     io.AddPeripheral(0x3c0, 31, *this);
+}
+
+void VGA::Impl::Update()
+{
+    for (unsigned int y = 0; y < 25; y++)
+        for (unsigned int x = 0; x < 80; x++) {
+            const auto ch = videomem[160 * y + 2 * x + 0];
+            [[maybe_unused]] const auto cl = videomem[160 * y + 2 * x + 1];
+            const auto d = &font_data[ch * 8];
+            for (unsigned int j = 0; j < 8; j++)
+                for (unsigned int i = 0; i < 8; i++) {
+                    uint32_t color = 0x00ffffff;
+                    if ((d[j] & (1 << (8 - i))) == 0)
+                        color = 0;
+                    hostio.putpixel(x * 8 + i, y * 8 + j, color);
+                }
+        }
+}
+
+VGA::VGA(Memory& memory, IO& io, HostIO& hostio)
+    : impl(std::make_unique<Impl>(memory, io, hostio))
+{
 }
 
 VGA::~VGA() = default;
 
 void VGA::Reset()
 {
-    memset(m_videomem.get(), 0, VideoMemorySize);
+    std::fill(impl->videomem.begin(), impl->videomem.end(), 0);
 }
 
-uint8_t VGA::ReadByte(Memory::Address addr)
+uint8_t VGA::Impl::ReadByte(Memory::Address addr)
 {
     spdlog::info("vga: read(8) @ 0x{:4x}", addr);
     if (addr >= 0xb8000 && addr <= 0xb8fff) {
-        return m_videomem[addr - 0xb8000];
+        return videomem[addr - 0xb8000];
     }
     return 0;
 }
 
-uint16_t VGA::ReadWord(Memory::Address addr)
+uint16_t VGA::Impl::ReadWord(Memory::Address addr)
 {
     spdlog::info("vga: read(16) @ 0x{:4x}", addr);
-    return ReadByte(addr) | ReadByte(addr + 1) << 8;
-}
-
-void VGA::WriteByte(Memory::Address addr, uint8_t data)
-{
-    spdlog::info("vga: write(8) @ 0x{:4x} data=0x{:04x}", addr, data);
-    if (addr >= 0xb8000 && addr <= 0xb8fff) {
-        m_videomem[addr - 0xb8000] = data;
+    if (addr >= 0xb8000 && addr <= 0xb8fff - 1) {
+        const auto a = videomem[addr - 0xb8000 + 0];
+        const auto b = videomem[addr - 0xb8000 + 1];
+        return a | (static_cast<uint16_t>(b) << 8);
+    } else {
+        return 0;
     }
 }
 
-void VGA::WriteWord(Memory::Address addr, uint16_t data)
+void VGA::Impl::WriteByte(Memory::Address addr, uint8_t data)
 {
-    spdlog::info("vga: write(16) @ 0x{:4x} data=0x{:04x}", addr, data);
-    WriteByte(addr, data & 0xff);
-    WriteByte(addr + 1, data >> 8);
+    spdlog::info("vga: write(8) @ 0x{:4x} data=0x{:04x}", addr, data);
+    if (addr >= 0xb8000 && addr <= 0xb8fff) {
+        videomem[addr - 0xb8000] = data;
+    }
 }
 
-void VGA::Out8(io_port port, uint8_t val)
+void VGA::Impl::WriteWord(Memory::Address addr, uint16_t data)
+{
+    spdlog::info("vga: write(16) @ 0x{:4x} data=0x{:04x}", addr, data);
+    if (addr >= 0xb8000 && addr <= 0xb8fff - 1) {
+        videomem[addr - 0xb8000 + 0] = data & 0xff;
+        videomem[addr - 0xb8000 + 1] = data >> 8;
+    }
+}
+
+void VGA::Impl::Out8(io_port port, uint8_t val)
 {
     spdlog::info("vga: out8({:x}, {:x}", port, val);
 }
 
-uint8_t VGA::In8(io_port port)
+uint8_t VGA::Impl::In8(io_port port)
 {
     spdlog::info("vga: in8({:x})", port);
     switch(port) {
@@ -101,12 +153,12 @@ uint8_t VGA::In8(io_port port)
     return 0;
 }
 
-void VGA::Out16(io_port port, uint16_t val)
+void VGA::Impl::Out16(io_port port, uint16_t val)
 {
     spdlog::info("vga: out16({:x}, {:x}", port, val);
 }
 
-uint16_t VGA::In16(io_port port)
+uint16_t VGA::Impl::In16(io_port port)
 {
     spdlog::info("vga: in16({:x})", port);
     return 0;
@@ -114,17 +166,5 @@ uint16_t VGA::In16(io_port port)
 
 void VGA::Update()
 {
-    for (unsigned int y = 0; y < 25; y++)
-        for (unsigned int x = 0; x < 80; x++) {
-            const auto ch = m_videomem[160 * y + 2 * x + 0];
-            [[maybe_unused]] const auto cl = m_videomem[160 * y + 2 * x + 1];
-            const auto d = &font_data[ch * 8];
-            for (unsigned int j = 0; j < 8; j++)
-                for (unsigned int i = 0; i < 8; i++) {
-                    uint32_t color = 0x00ffffff;
-                    if ((d[j] & (1 << (8 - i))) == 0)
-                        color = 0;
-                    m_hostio.putpixel(x * 8 + i, y * 8 + j, color);
-                }
-        }
+    impl->Update();
 }
--- src/vga.h
+++ src/vga.h
@@ -2,43 +2,26 @@
 #define __VGA_H__
 
 #include <memory>
-#include "memory.h"
-#include "io.h"
 
-class IO;
 class HostIO;
-class Vectors;
+class IO;
+class Memory;
 
-class VGA : XMemoryMapped, IOPeripheral
+class VGA final
 {
-  public:
+    struct Impl;
+    std::unique_ptr<Impl> impl;
+
+public:
     VGA(Memory& memory, IO& io, HostIO& hostio);
-    virtual ~VGA();
+    ~VGA();
 
     virtual void Reset();
     virtual void Update();
 
-    uint8_t ReadByte(Memory::Address addr) override;
-    uint16_t ReadWord(Memory::Address addr) override;
-
-    void WriteByte(Memory::Address addr, uint8_t data) override;
-    void WriteWord(Memory::Address addr, uint16_t data) override;
-
-    void Out8(io_port port, uint8_t val) override;
-    void Out16(io_port port, uint16_t val) override;
-    uint8_t In8(io_port port) override;
-    uint16_t In16(io_port port) override;
-
     // XXX Resolution for now
     static constexpr inline unsigned int s_video_width = 640;
     static constexpr inline unsigned int s_video_height = 400;
-
-  protected:
-    std::unique_ptr<uint8_t[]> m_videomem;
-
-    HostIO& m_hostio;
-    Memory& m_memory;
-    IO& m_io;
 };
 
 #endif /* __VGA_H__ */
